2708f0e101dc5bd4240a555af9e53327eedc4e35
Lara Peddinghausen <larapedd@gmail.com>
2026-03-30 20:19:33 -0300

    feat(medico): implementa listagem de médicos com DTO e paginação

--- src/main/java/com/github/app/controller/MedicoController.java
+++ src/main/java/com/github/app/controller/MedicoController.java
@@ -1,7 +1,11 @@
 package com.github.app.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;  //já importa todos os que estão comentados abaixo
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*; //já importa todos os que estão comentados abaixo
 
 // import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.RequestBody;
@@ -9,18 +13,42 @@ import org.springframework.web.bind.annotation.*;  //já importa todos os que es
 // import org.springframework.web.bind.annotation.RestController;
 
 import com.github.app.model.medico.DadosCadastroMedico;
+import com.github.app.model.medico.DadosListagemMedico;
 import com.github.app.model.medico.Medico;
 import com.github.app.model.medico.MedicoRepository;
 
-@RestController //SPRING WEB - Informa para o Springboot que abaixo é uma classe controladora de requisições (GET-POST-PUT-DELETE)
-@RequestMapping("/medicos") //SPRING WEB - Cria um caminho (endpoint) para a classe MedicoCotroller
+@RestController // SPRING WEB - Informa para o Springboot que abaixo é uma classe controladora
+                // de requisições (GET-POST-PUT-DELETE)
+@RequestMapping("/medicos") // SPRING WEB - Cria um caminho (endpoint) para a classe MedicoCotroller
 public class MedicoController {
 
-    @Autowired //Sobreescrevendo algo. É um padrão utilizado na injeção de dependência.
-    private MedicoRepository repository; //tem que importar
-    
-    @PostMapping //SPRING WEB - Informa que o método abaixo é do tipo POST (cadastrar)
+    @Autowired // Sobreescrevendo algo. É um padrão utilizado na injeção de dependência.
+    private MedicoRepository repository; // tem que importar
+
+    @PostMapping // SPRING WEB - Informa que o método abaixo é do tipo POST (cadastrar)
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+
     }
+
+    @GetMapping("todos") // SRPING WEB - Informa que o método abaixo é do tipo GET (buscar/ler)
+    public List<Medico> listarTodos() { // List tem que importar java.util.List
+        return repository.findAll();
+    }
+
+    @GetMapping("listar") 
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();  //mapeando o que está em DadosListagemMedico e convertendo para uma lista
+        // findAll() -> método que retorna uma lista de objetos do tipo DadosListagemMedico
+        // stream() -> método utilizado para transformar a lista em um fluxo de daodos, permitindo aplicar operações de transformação
+        // map(DadosListagemMedico::new) -> método utilizado para converter cada objeto do tipo medico em json DadosListagemMedico, utilzando o constructor que criamos em DadosListagemMedico
+        // toList() -> método utilizado para coletar os resultados em uma nova lista do tipo DadosListagemMedico, que é o formato que queremos retornar para a API
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listarPorPagina(Pageable paginacao) { //Page tem que importar o do domain (org.springframework.data.domain.Page). Pageable também.
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+        //return repository.findAll().stream().map(DadosListagemMedico::new).toList(); 
+    }
+
 }
