56dbd594fafcb79e80316c15418965a4c0d263da
Lara Peddinghausen <larapedd@gmail.com>
2026-04-29 19:46:07 -0300

    feat(consulta): implementa agendamento de consultas com DTO (estrutura inicial)

--- src/main/java/com/github/app/controller/ConsultaController.java
+++ src/main/java/com/github/app/controller/ConsultaController.java
@@ -1,5 +1,42 @@
 package com.github.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.github.app.model.consulta.Consulta;
+import com.github.app.model.consulta.ConsultaRepository;
+import com.github.app.model.consulta.DadosAgendamentoConsulta;
+import com.github.app.model.medico.MedicoRepository;
+import com.github.app.model.paciente.PacienteRepository;
+
+@RestController
+@RequestMapping("/consultas")
 public class ConsultaController {
     
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    // Cadastro de uma nova consulta
+    @PostMapping
+    public Consulta agendar(@RequestBody DadosAgendamentoConsulta dados) {
+        // pegar o id do médico
+        var medico = medicoRepository.getReferenceById(dados.medicoId());
+
+        // pegar o id do paciente
+        var paciente = pacienteRepository.getReferenceById(dados.pacienteId());
+
+        // agendar a consulta com o paciente e o médico buscados
+        var consulta = new Consulta(dados);
+        // consulta.setMedico(medico);
+        // consulta.setPaciente(paciente);
+
+        return consultaRepository.save(consulta);
+    }
+
 }
--- src/main/java/com/github/app/model/consulta/Consulta.java
+++ src/main/java/com/github/app/model/consulta/Consulta.java
@@ -23,11 +23,11 @@ public class Consulta {
 
     @JoinColumn(name = "medicoId") // Define o nome da coluna "alias" na tabela consulta, ou seja, pega a PK do médico e transforma em FK em consulta com o nome da coluna de medicoId
     @ManyToOne
-    private Medico medico;
+    private Integer medico;
 
     @JoinColumn(name = "pacienteId")
     @ManyToOne
-    private Paciente paciente;
+    private Integer paciente;
 
     @Enumerated(EnumType.STRING)
     private Status status;
@@ -36,5 +36,13 @@ public class Consulta {
 
     private LocalDateTime data;
 
+    // Terceiro constructor da classe Consulta que recebe a conversão que a classe DTO DadosAgendamentoConsulta está realizando. Json -> OBJ(dict)
+    public Consulta(DadosAgendamentoConsulta dados) {
+        this.medico = dados.medicoId();
+        this.paciente = dados.pacienteId();
+        this.status = dados.status();
+        this.observacao = dados.observacao();
+        this.data = dados.data();
+    }
 
 }
