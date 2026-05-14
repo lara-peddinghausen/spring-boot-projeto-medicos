a4a6552f2e24f61232d7181bd6b1c24ee32f4265
Lara Peddinghausen <larapedd@gmail.com>
2026-05-14 19:04:11 -0300

    refactor(paciente): atualiza DTO de listagem de pacientes

--- src/main/java/com/github/app/model/paciente/DadosListagemPaciente.java
+++ src/main/java/com/github/app/model/paciente/DadosListagemPaciente.java
@@ -1,12 +1,12 @@
 package com.github.app.model.paciente;
 
 public record DadosListagemPaciente(
-    Integer id,
     String nome,
-    String telefone
+    String email,
+    String cpf
 ) {
     public DadosListagemPaciente(Paciente paciente) {
-        this(paciente.getId(), paciente.getNome(), paciente.getTelefone());
+        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
     }
     
 }
